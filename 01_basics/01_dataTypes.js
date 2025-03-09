6db01b0b0756b0407d2616c3ef58f0f4d985bbcb
SidhappaBhusangi <sidhappabhusangi@gmail.com>
2025-03-09 16:32:40 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert (3+3)  // we are using node.js, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("Sidhappa");
+
+
+// data types
+let name = "sidhappa";      // string => ""
+let age = 33;                // number => 2 to power 53
+let isLoggedIn = false;      // boolean => true/false
+
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+
