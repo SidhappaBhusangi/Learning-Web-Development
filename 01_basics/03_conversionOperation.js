03bd76bdce7aeeed738cde9062e0ec456a437722
SidhappaBhusangi <sidhappabhusangi@gmail.com>
2025-03-10 02:42:25 +0000

    conversion of values

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,114 @@
+// let score = 33;
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// //conversion of complex value like 33aa (not a string and not a number)
+// let score = "33aa";
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); //score (33aa) will be converted in number
+// console.log(typeof (valueInNumber)); // typeof will show as a number
+// console.log(valueInNumber); //but value will be NaN means Not a Number
+
+
+
+// //conversion of null value
+// let score = null;
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); //score (33aa) will be converted in number
+// console.log(typeof (valueInNumber)); // typeof will show as a number
+// console.log(valueInNumber); //but value will be "0"
+
+
+
+// //conversion of undefined value
+// let score = undefined;
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); //score (33aa) will be converted in number
+// console.log(typeof (valueInNumber)); // typeof will show as a number
+// console.log(valueInNumber); //but value will be shown as NaN e.i. Not a Number
+
+
+
+
+
+// //conversion of boolean value
+// let score = true;
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); //score (33aa) will be converted in number
+// console.log(typeof (valueInNumber)); // typeof will show as a number
+// console.log(valueInNumber); //but value will be shown as "1" for true and "0" for false.
+
+
+
+
+// //conversion of string value
+// let score = "sidhappa";
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score); //score (33aa) will be converted in number
+// console.log(typeof (valueInNumber)); // typeof will show as a number
+// console.log(valueInNumber); //but value will be shown as NaN (Not a Number)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+
+// //conversion of boolean value e.i. 0 & 1
+// let isLoggedIn = 1
+
+// // let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn); // value will show as "true"
+
+
+
+// //conversion of boolean value e.i. 0 & 1
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // value will show as "false"
+
+
+
+// //conversion of boolean value e.i. 0 & 1
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // value will show as "false"
+
+
+
+// //conversion of boolean value e.i. 0 & 1
+// let isLoggedIn = "sidhappa"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // value will show as "true"
+
+
+
+// 1 => true;  0 => false
+// "" => false
+// "sidhappa" => true
+
+
