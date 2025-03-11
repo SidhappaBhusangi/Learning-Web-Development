0e96183a559582117c33aa8f6cf2d31c072aa4b4
SidhappaBhusangi <sidhappabhusangi@gmail.com>
2025-03-11 14:56:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// // comparisions
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);  result will "true" becuase string is getting converted in number and 2 is greater 1 so result is "True"
+// console.log("02">1); result will "true" becuase string is getting converted in number and 2 is greater 1 so result is "True"
+
+// console.log(null > 0) result is false 
+// console.log(null <= 0) result is false
+// console.log(null == 0) result is true i.e. because null is converted into 0. no fix rule. null will be converted into "0" or "NaN"
+
+// same with undefined
+// console.log(undefined > 0) result is false 
+// console.log(undefined <= 0) result is false
+// console.log(undefined == 0) result is false unlike comparison with null. 
+
+
+// so best method to compare values is strict check i.e. === (triple "=")
