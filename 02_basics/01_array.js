5ba7ed119c6571030e8bf62a60e7802985639812
SidhappaBhusangi <sidhappabhusangi@gmail.com>
2025-03-18 02:44:00 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+// array
+
+const myArr = [1,2,3,4,5,7,8,"Sidhappa",true];
+const myHeroes = ["superman", "batman","hulk", "spiderman"];
+
+// console.log(myArr, myHeroes);
+myHeroes.push("Mr. Doctor","shaktiman")
+myHeroes.unshift("aryaman")
+console.log(myHeroes);
+
