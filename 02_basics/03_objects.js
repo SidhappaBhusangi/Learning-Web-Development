190f37927ad568cf093ef1eb2615b52a1296116f
SidhappaBhusangi <sidhappabhusangi@gmail.com>
2025-03-19 01:56:16 +0000

    object basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+//singleton
+//Object.create   ---constructor method (creates singleton)
+
+// object literals
+
+const JsUser = {
+    name : "sidhappa",
+    age : 33,
+    city : "pune",
+    pincode: "411023",
+    email: "sid@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+};
